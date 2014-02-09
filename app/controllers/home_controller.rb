af72a8337a329c93d36b85d03e9d14dc027d893b
Helena Freire <helenamcfreire@gmail.com>
2014-02-09 00:06:32 -0200

    finally

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,9 @@ class HomeController < ApplicationController
       @estrofe3 = Estrofe3.random.title
       @estrofe4 = Estrofe4.random.title
       @estrofe5 = Estrofe5.random.title
+      @conector1 = [t('pega'), t('solta'), t('tras'), t('lanca_um')].sample
       @estrofe6 = Estrofe6.random.title
+      @conector2 = [t('e'), t('e_agora')].sample
       @estrofe7 = Estrofe7.random.title
 
   end
